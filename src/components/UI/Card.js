08a14486d916dade030ea1e9a0c8150d205da6ab
Fargol Shirvanifar <fargolsh79@gmail.com>
2023-05-04 22:20:59 +0430

    config react & sass, add card component

---
+++ src/components/UI/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Card.module.scss';
+
+const Card = (props) => {
+    return (
+        <div className={classes.card}>
+            <p>hi there!</p>
+        </div>
+    );
+}
+
+export default Card;
