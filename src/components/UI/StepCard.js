d3c9063237609ccbf911570384fbbda5c9a1f5b9
Fargol Shirvanifar <fargolsh79@gmail.com>
2023-05-11 22:01:40 +0430

    Complete step card styling

--- src/components/UI/StepCard.js
+++ src/components/UI/StepCard.js
@@ -5,31 +5,42 @@ const StepCard = () => {
     return (
         <div className={classes['step-card']}>
             <div className={classes['step-card__group']}>
-                <span>1</span>
-                <div>
-                    <p>STEP 1</p>
-                    <p>YOUR INFO</p>
+                <div className={classes['step-card__radio']}>
+                    <span className={classes['step-card__number']}>1</span>
+                </div>
+                <div className={classes['step-card__text-group']}>
+                    <p className={classes['step-card__label']}>STEP 1</p>
+                    <p className={classes['step-card__title']}>YOUR INFO</p>
                 </div>
             </div>
+
             <div className={classes['step-card__group']}>
-                <span>2</span>
-                <div>
-                    <p>STEP 2</p>
-                    <p>SELECT PLAN</p>
+                <div className={classes['step-card__radio']}>
+                    <span className={classes['step-card__number']}>2</span>
+                </div>
+                <div className={classes['step-card__text-group']}>
+                    <p className={classes['step-card__label']}>STEP 2</p>
+                    <p className={classes['step-card__title']}>SELECT PLAN</p>
                 </div>
             </div>
+
             <div className={classes['step-card__group']}>
-                <span>3</span>
-                <div>
-                    <p>STEP 3</p>
-                    <p>ADD ONS</p>
+                <div className={classes['step-card__radio']}>
+                    <span className={classes['step-card__number']}>3</span>
+                </div>
+                <div className={classes['step-card__text-group']}>
+                    <p className={classes['step-card__label']}>STEP 3</p>
+                    <p className={classes['step-card__title']}>ADD-ONS</p>
                 </div>
             </div>
+
             <div className={classes['step-card__group']}>
-                <span>4</span>
-                <div>
-                    <p>STEP 4</p>
-                    <p>SUMMARY</p>
+                <div className={classes['step-card__radio']}>
+                    <span className={classes['step-card__number']}>4</span>
+                </div>
+                <div className={classes['step-card__text-group']}>
+                    <p className={classes['step-card__label']}>STEP 4</p>
+                    <p className={classes['step-card__title']}>SUMMARY</p>
                 </div>
             </div>
         </div>
